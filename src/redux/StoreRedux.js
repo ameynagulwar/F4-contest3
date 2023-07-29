230b006fd36fee5625011982ca913f23261793ad
ameynagulwar <130839577+ameynagulwar@users.noreply.github.com>
2023-07-29 23:28:59 +0530

    first

---
+++ src/redux/StoreRedux.js
@@ -0,0 +1,7 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from './ReducersRedux';
+const storeRedux = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export default storeRedux;
+
+// commit
